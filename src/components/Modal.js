a7fb829c6d0c6c3495cff75b5ea2c7a118b3a4c3
Kevin Jake <kevin_jake_04@yahoo.com>
2022-09-30 15:02:08 +0800

    add data on the modal dialog and close button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Form from "./Form";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const Modal = ({ showModal, setShowModal, data }) => {
+  const properties = Object.getOwnPropertyNames(data);
   if (!showModal) return null;
   console.log(data);
   const handleOnClose = (e) => {
@@ -15,10 +17,27 @@ const Modal = ({ showModal, setShowModal, data }) => {
       className="fixed inset-0 z-20 flex justify-center items-center bg-black bg-opacity-25 backdrop-blur-sm "
     >
       <div className="w-[600px]">
+        <AiOutlineCloseCircle
+          className="cursor-pointer float-right "
+          size={23}
+          onClick={() => setShowModal(false)}
+        />
         <div className="bg-white p-2 rounded">
-          <h1 className="text-lg font-bold p-4">Battery</h1>
+          <h1 className="text-lg font-bold p-4">{data.__typename}</h1>
           <div className="grid grid-flow-row">
-            <Form />
+            <div>
+              <ul>
+                {properties.map((prop) => (
+                  <li key={prop}>
+                    <div className="grid grid-cols-2">
+                      <p className="font-bold text-sm">{prop}</p>
+                      <p className="text-sm">{data[prop]}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* <Form /> */}
           </div>
         </div>
       </div>
