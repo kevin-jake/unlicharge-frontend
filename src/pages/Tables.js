7244c551440973217d4ff6c2f5ab9ac31fbc9c91
Kevin Jake <kevin_jake_04@yahoo.com>
2022-10-04 14:45:45 +0800

    uninstall syncfusion grid component

--- src/pages/Tables.js
+++ src/pages/Tables.js
@@ -1,5 +1,4 @@
 import { useQuery } from "@apollo/client";
-import { GridComponent } from "@syncfusion/ej2-react-grids";
 import React from "react";
 import { FETCH_BATTERY } from "../util/graphql/Query";
 
@@ -9,13 +8,7 @@ const Tables = () => {
   return (
     <>
       {/* TODO: Change to loading spinner */}
-      {loading ? (
-        <h1>Loading posts...</h1>
-      ) : (
-        <div>
-          <GridComponent dataSource={data.getBatteries}></GridComponent>
-        </div>
-      )}
+      {loading ? <h1>Loading posts...</h1> : <div></div>}
       <button
         // onClick={() => openModal(item)}
         className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600"
