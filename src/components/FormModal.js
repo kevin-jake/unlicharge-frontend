05147a2d50de8e35b00a2d19d569a0642c4b527d
Kevin Jake <kevin_jake_04@yahoo.com>
2022-10-06 17:02:59 +0800

    dynamic text fields

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -1,67 +1,78 @@
 import React from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Modal from "./Modal";
+import { defaultDataIdFromObject } from "@apollo/client";
 
 const FormModal = ({ showFormModal, setShowFormModal, formData }) => {
+  const properties = Object.getOwnPropertyNames(formData);
+  const formDisplay = ["id", "__typename", "createdAt", "publish_status"];
+
+  const format = (string) => {
+    var cleanString = string.replaceAll("_", " ");
+    return cleanString.charAt(0).toUpperCase() + cleanString.slice(1);
+  };
+
+  const inputField = (prop, formData) => {
+    switch (prop) {
+      case "name": {
+        return (
+          <div>
+            <TextField
+              fullWidth
+              id={"form" + prop}
+              label={format(prop)}
+              defaultValue={formData[prop]}
+              helperText={formData[prop]}
+            />
+          </div>
+        );
+      }
+      default: {
+        return (
+          <TextField
+            id={"form" + prop}
+            label={format(prop)}
+            defaultValue={formData[prop]}
+            helperText={formData[prop]}
+          />
+        );
+      }
+    }
+  };
+
   return (
     <Modal
       showModal={showFormModal.open}
-      title={formData.name}
+      title={
+        showFormModal.operation +
+        " " +
+        formData.__typename +
+        " " +
+        formData.name
+      }
       closeModal={() => setShowFormModal({ ...showFormModal, open: false })}
     >
       <Box
         component="form"
         sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
+          "& .MuiTextField-root": { m: 1 },
         }}
         noValidate
         autoComplete="off"
       >
-        <Typography variant="h5">
-          {showFormModal.operation + " " + formData.__typename}
-        </Typography>
         <div>
-          <TextField
-            required
-            id="outlined-required"
-            label="Required"
-            defaultValue="Hello World"
-          />
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            defaultValue="Hello World"
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-          />
-          <TextField
-            id="outlined-read-only-input"
-            label="Read Only"
-            defaultValue="Hello World"
-            InputProps={{
-              readOnly: true,
-            }}
-          />
-          <TextField
-            id="outlined-number"
-            label="Number"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField id="outlined-search" label="Search field" type="search" />
-          <TextField
-            id="outlined-helperText"
-            label="Helper text"
-            defaultValue="Default Value"
-            helperText="Some important text"
-          />
+          {properties.map(
+            (prop) => !formDisplay.includes(prop) && inputField(prop, formData)
+          )}
         </div>
       </Box>
     </Modal>
