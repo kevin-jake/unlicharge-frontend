65cc42f080dfd1141c32272d1267a1963a17a616
Kevin Jake <kevin_jake_04@yahoo.com>
2023-02-22 10:14:07 +0800

    fix layout of CategoryCards

--- src/scenes/BuildPage/CategoryCards.jsx
+++ src/scenes/BuildPage/CategoryCards.jsx
@@ -1,41 +1,52 @@
 import {
+  Battery1BarOutlined,
   ChatBubbleOutlineOutlined,
   FavoriteBorderOutlined,
   FavoriteOutlined,
   ShareOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, Grid, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Divider,
+  Grid,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import FlexBetween from "../../components/FlexBetween";
 import Friend from "../../components/Friend";
 import WidgetWrapper from "../../components/WidgetWrapper";
 
 const CategoryCards = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { palette } = useTheme();
+
   return (
     <Grid item xs={4} padding="1rem">
-      <WidgetWrapper>
-        {/* <Friend /> */}
-        <Typography sx={{ mt: "1rem" }}> Test</Typography>
-        <FlexBetween mt="0.25rem">
-          {/* <FlexBetween gap="1rem">
-            <FlexBetween gap="0.3rem">
-              <IconButton></IconButton>
-              <Typography></Typography>
+      <Card
+        sx={{
+          backgroundColor: palette.background.alt,
+          borderRadius: "0.75rem",
+        }}
+      >
+        <CardActionArea>
+          <CardContent>
+            <FlexBetween mt="0.25rem" sx={{ justifyContent: "center" }}>
+              <FlexBetween mt="0.25rem">
+                <Battery1BarOutlined fontSize="large" />
+                <Typography variant="h4" sx={{ marginLeft: "0.25rem" }}>
+                  Battery
+                </Typography>
+              </FlexBetween>
             </FlexBetween>
-
-            <FlexBetween gap="0.3rem">
-              <IconButton>
-                <ChatBubbleOutlineOutlined />
-              </IconButton>
-              <Typography></Typography>
-            </FlexBetween>
-          </FlexBetween> */}
-
-          <IconButton>
-            <ShareOutlined />
-          </IconButton>
-        </FlexBetween>
-      </WidgetWrapper>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Grid>
   );
 };
