207c4568ced8a9073be3fa148d58f09341fb4791
Kevin Jake <kevin_jake_04@yahoo.com>
2023-03-28 11:48:30 +0800

    change layout of page footer

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -11,7 +11,13 @@ import {
 import React from "react";
 import FlexBetween from "./wrappers/FlexBetween";
 
-const PageFooter = ({ page = 1, total = 100, limit = 5, setPagination }) => {
+const PageFooter = ({
+  page = 1,
+  total = 100,
+  limit = 5,
+  setPagination,
+  isShown,
+}) => {
   const pageNumbers = Math.ceil(total / limit);
   const handlePageChange = (event) => {
     setPagination(event.target.outerText, limit);
@@ -21,42 +27,47 @@ const PageFooter = ({ page = 1, total = 100, limit = 5, setPagination }) => {
   };
   return (
     <>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Pagination
-          count={pageNumbers}
-          page={page}
-          onChange={handlePageChange}
-        />
-        <FlexBetween>
-          <Typography variant="caption" sx={{ margin: "0.25rem" }}>
-            Page {page} of {pageNumbers}
-          </Typography>
-          <Divider orientation="vertical" marginX="1rem" />
-          <Typography variant="caption" sx={{ margin: "0.25rem" }}>
-            Total Results: {total}
-          </Typography>
-        </FlexBetween>
+      {isShown && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Pagination
+            count={pageNumbers}
+            page={page}
+            onChange={handlePageChange}
+          />
+          <FlexBetween>
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id="numberOfResults-label">
+                Results per page:
+              </InputLabel>
+              <Select
+                labelId="numberOfResults-label"
+                id="numberOfResults-label"
+                value={limit}
+                label="Results per page:"
+                onChange={handleLimitChange}
+              >
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+              </Select>
+            </FormControl>
+            <Divider orientation="vertical" variant="middle" flexItem />
 
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-          <InputLabel id="numberOfResults-label">Results per page:</InputLabel>
-          <Select
-            labelId="numberOfResults-label"
-            id="numberOfResults-label"
-            value={limit}
-            label="Results per page:"
-            onChange={handleLimitChange}
-          >
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+            <Typography variant="caption" sx={{ margin: "0.5rem 1rem" }}>
+              Page {page} of {pageNumbers}
+            </Typography>
+            <Divider orientation="vertical" variant="middle" flexItem />
+            <Typography variant="caption" sx={{ margin: "0.5rem 1rem" }}>
+              Total Results: {total}
+            </Typography>
+          </FlexBetween>
+        </Box>
+      )}
     </>
   );
 };
