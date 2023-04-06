682186e40956bd181c6ecfa2c3955c2a2ce2e604
Kevin Jake <kevin_jake_04@yahoo.com>
2023-04-06 08:52:51 +0800

    add total Watt hours and fix kilo displays

--- src/util/numberFormats.js
+++ src/util/numberFormats.js
@@ -2,17 +2,17 @@ const numberWithCommas = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
-const kiloformat = (num) => {
+const kiloformat = (num, unit) => {
   if (num >= 1000000000) {
-    return (num / 1000000000).toFixed(3).replace(/\.0$/, "") + " G";
+    return (num / 1000000000).toFixed(3).replace(/\.0$/, "") + " G" + unit;
   }
   if (num >= 1000000) {
-    return (num / 1000000).toFixed(3).replace(/\.0$/, "") + " M";
+    return (num / 1000000).toFixed(3).replace(/\.0$/, "") + " M" + unit;
   }
   if (num >= 1000) {
-    return (num / 1000).toFixed(3).replace(/\.0$/, "") + " k";
+    return (num / 1000).toFixed(3).replace(/\.0$/, "") + " k" + unit;
   }
-  return num;
+  return num + " " + unit;
 };
 
 export { numberWithCommas, kiloformat };
